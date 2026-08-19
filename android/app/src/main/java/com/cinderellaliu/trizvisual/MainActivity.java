d952474e4dfb610a20593ac8a9c12d5570b3e3d2
cinderella-liu <942303798@qq.com>
2026-08-20 07:03:58 +0800

    feat: add Android wrapper for TRIZ app

---
+++ android/app/src/main/java/com/cinderellaliu/trizvisual/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cinderellaliu.trizvisual;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cinderellaliu.trizvisual',
+  appName: 'TRIZ Visual Analysis',
+  webDir: 'dist'
+};
+
+export default config;
